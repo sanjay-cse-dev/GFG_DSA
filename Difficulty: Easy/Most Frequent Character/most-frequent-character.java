2848ea55d436732c09b7d5a20fdf82b963c125bd
Sanjay Kumar <sanjaykumar.cse.dev@gmail.com>
2026-04-11 20:46:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Most Frequent Character/most-frequent-character.java
+++ Difficulty: Easy/Most Frequent Character/most-frequent-character.java
@@ -1,21 +1,57 @@
 class Solution {
     public char getMaxOccuringChar(String s) {
+        int n = s.length();
+        int[] freq = new int[26];
         
-        int maxfreq = -1;
-        char ans = s.charAt(0);
-        
-        for(int i=0; i<s.length(); i++){
-            int freq = 0; 
+        for(int i=0; i<n; i++){
             char ch = s.charAt(i);
-            for(int j=i+1; j<s.length(); j++){
-                if(s.charAt(j)==ch) freq++;
+            int idx = ch-97;
+            freq[idx]++;
+        }
+        int maxFreq = 0;
+        char ans = s.charAt(0);
+        for(int i=0; i<26; i++){
+            if(freq[i]>maxFreq){
+                maxFreq = freq[i];
+                ans = (char)(i+97);
             }
-            if(maxfreq < freq){
-                maxfreq = freq;
-                ans = ch;
-            }
-            else if(maxfreq == freq && ch < ans) ans = ch;
         }
         return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // char[] arr = s.toCharArray();
+        // Arrays.sort(arr);
+        // int n = arr.length;
+        // int maxfreq = 0; int freq = 0;
+        // int i = 0, j = 0;
+        // char ans = arr[0];
+        // while(j<n){
+        //     if(arr[i] != arr[j]){
+        //         freq = j-i;
+        //         if(freq>maxfreq){
+        //             maxfreq = freq;
+        //             ans = arr[i];
+        //         }
+        //         i=j;
+        //     }else j++;
+        // }
+        // freq = j-i;
+        // if(freq>maxfreq){
+        //     maxfreq = freq;
+        //     ans = arr[i];
+        // }
+        // return ans;
     }
 }
