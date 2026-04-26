eb1eddf9355cf7869be772b721134e2aed19d501
Sanjay Kumar <sanjaykumar.cse.dev@gmail.com>
2026-04-26 10:23:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Odd and Even/count-odd-and-even.java
+++ Difficulty: Basic/Count Odd and Even/count-odd-and-even.java
@@ -1,17 +1,68 @@
 class Solution {
     public int[] countOddEven(int[] arr) {
-        // Code here
-        int even = 0;
-        int odd = 0;
+        int evenCount=0; int oddCount=0;
         for(int i=0; i<arr.length; i++){
-            if(arr[i]%2 == 0) even += 1;
-            else{
-                odd += 1;
-            }
+            if(arr[i]%2==0) evenCount++;
+            else oddCount++;
         }
-        int[] result = new int[2];
-        result[1] = even;
-        result[0] = odd;
-        return result;
+        int[] ans = new int[2];
+        ans[0] = oddCount;
+        ans[1] = evenCount;
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Code here
+        // int even = 0;
+        // int odd = 0;
+        // for(int i=0; i<arr.length; i++){
+        //     if(arr[i]%2 == 0) even += 1;
+        //     else{
+        //         odd += 1;
+        //     }
+        // }
+        // int[] result = new int[2];
+        // result[1] = even;
+        // result[0] = odd;
+        // return result;
+        
+        
     }
 }
